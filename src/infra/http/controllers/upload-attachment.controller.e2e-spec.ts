393a027c47165659f7d4e7ce59e5d31271e17137
Igor Lacerda <igaum@IGOR.localdomain>
2023-09-23 17:45:52 -0300

    :feat: criando caso de uso de upload e test do mesmo

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+import { QuestionFactory } from 'test/factories/make-question';
+import { StudentFactory } from 'test/factories/make-students';
+
+describe('Upload attachment (E2E)', () => {
+  let app: INestApplication;
+  let jwt: JwtService;
+  let studentFactory: StudentFactory;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    jwt = moduleRef.get(JwtService);
+
+    studentFactory = moduleRef.get(StudentFactory);
+
+    await app.init();
+  });
+
+  test('[POST] /attachments', async () => {
+    const user = await studentFactory.makePrismaStudent();
+
+    const access_token = jwt.sign({ sub: user.id.toString() });
+
+    const response = await request(app.getHttpServer())
+      .get('/attachments')
+      .set('Authorization', `Bearer ${access_token}`)
+      .attach('file', './test/e2e/sample-upload.jpeg');
+
+    expect(response.statusCode).toBe(201);
+  });
+});
---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('/attachments')
+export class UploadAttachmentsController {
+  //   constructor(private getQuestionBySlug: GetQuestionBySlugUseCase) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 2, // 2mb
+          }),
+          new FileTypeValidator({ fileType: '.(png|jpg|jpeg|pdf)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+  }
+}
