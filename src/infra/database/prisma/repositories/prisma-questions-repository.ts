ca025e7eb80010ba7010f50d8cb9ddfcbafa6885
Igor Lacerda <igaum@IGOR.localdomain>
2023-10-16 17:25:43 -0300

    :feat: criando cache utilizando redis

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -8,11 +8,13 @@ import { QuestionAttachmentRepository } from '@/domain/forum/application/reposit
 import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details';
 import { PrismaQuestionDetailsMapper } from '../mappers/prisma-question-details-mapper';
 import { DomainEvents } from '@/core/events/domain-events';
+import { CacheRepository } from '@/infra/cache/cache-repository';
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
   constructor(
     private prisma: PrismaService,
+    private cache: CacheRepository,
     private questionAttachmentsRepository: QuestionAttachmentRepository,
   ) {}
 
@@ -39,6 +41,13 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   public async findDetailsBySlug(
     slug: string,
   ): Promise<QuestionDetails | null> {
+    const cacheHit = await this.cache.get(`question:${slug}:details`);
+
+    if (cacheHit) {
+      const cachedData = JSON.parse(cacheHit);
+      return cachedData;
+    }
+
     const question = await this.prisma.question.findUnique({
       where: { slug },
       include: {
@@ -51,7 +60,14 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
       return null;
     }
 
-    return PrismaQuestionDetailsMapper.toDomain(question);
+    const questionDetails = PrismaQuestionDetailsMapper.toDomain(question);
+
+    await this.cache.set(
+      `question:${slug}:details`,
+      JSON.stringify(questionDetails),
+    );
+
+    return questionDetails;
   }
 
   public async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
@@ -112,6 +128,8 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
       this.questionAttachmentsRepository.deleteMany(
         question.attachments.getRemovedItems(),
       ),
+
+      this.cache.delete(`question:${data.slug}:details`),
     ]);
 
     DomainEvents.dispatchEventsForAggregate(question.id);
--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -4,18 +4,28 @@ import { PrismaClient } from '@prisma/client';
 import { randomUUID } from 'node:crypto';
 import { execSync } from 'node:child_process';
 import { DomainEvents } from '@/core/events/domain-events';
+import { Redis } from 'ioredis';
+import { envSchema } from '@/infra/env/env';
 
 config({ path: '.env', override: true });
 config({ path: '.env.test', override: true });
 
+const env = envSchema.parse(process.env);
+
 const prisma = new PrismaClient();
 
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+});
+
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('Please provider a DATABASE_URL enviroment variable.');
   }
 
-  const url = new URL(process.env.DATABASE_URL);
+  const url = new URL(env.DATABASE_URL);
 
   url.searchParams.set('schema', schemaId);
 
@@ -26,9 +36,12 @@ const schemaId = randomUUID();
 
 beforeAll(async () => {
   const databaseUrl = generateUniqueDatabaseURL(schemaId);
-  process.env.DATABASE_URL = databaseUrl;
+
+  env.DATABASE_URL = databaseUrl;
 
   DomainEvents.shouldRun = false;
+
+  await redis.flushdb();
 
   execSync('yarn prisma migrate deploy');
 });
